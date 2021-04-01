139eeb93561fb315c2b9dc70e6726e3e0e0d3b14
brian dominguez <briandv198@gmail.com>
2021-03-31 19:06:12 -0600

    Crear el Home

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,38 @@
 //Se instalaron las siguientes dependencias: @babel/core babel-loader html-webpack-plugin webpack webpack-cli webpack-dev-server
 
 //Viene en node y sirve para poder saber las direcciones de donde nos encontramos
-const path = require("path");
+const path = require('path');
 //Sirve para trabajar con webpack
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { resolve } = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  //Esta es la dirección de entrada de nuestro proyecto
-  entry: "./src/index.js",
-  //Aqui configuramos la salida, desde la carpeta hasta el nombre del archivo
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
-  resolve: {
-    extensions: [".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin([
-      {
-        inject: true,
-        template: "./public/index.html",
-        filname: "./index.html",
-      },
-    ]),
-  ],
-};
+    //Esta es la dirección de entrada de nuestro proyecto
+    entry: './src/index.js',
+    //Aqui configuramos la salida, desde la carpeta hasta el nombre del archivo
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module:{
+        rules:[{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebPackPlugin([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
+            }
+        ])
+    ]
+
+}
